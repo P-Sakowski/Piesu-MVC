8ad77f7710bddcf5a44955b99fbb44dddcd6893b
Przemysław Sakowski <p.sakowski@protonmail.com>
2020-12-29 17:45:35 +0100

    add controlers, basic views and dog model

---
+++ Piesu/Piesu.Web/Controllers/DogController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Piesu.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Piesu.Web.Controllers
+{
+    public class DogController : Controller
+    {
+        [Authorize(Roles = "Admin, Moderator, User")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Piesu/Piesu.Web/Models/DogModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Piesu.Web.Models
+{
+    public class DogModel
+    {
+        public string Name { get; set; }
+        //to-do: collection of breeds, possible to choose from list while adding new dog
+        public string Breed { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+}
